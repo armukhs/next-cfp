b862cbd8cfb0b06c8b6acbcfb6b3a2c102c6f367
muslax <muslax@gmail.com>
2023-03-07 20:43:03 +0700

    First commit

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+import type { NextRequest } from "next/server";
 
-type Data = {
-  name: string
-}
+export const config = {
+  runtime: "experimental-edge",
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(
+  req: NextRequest
+): Promise<Response> {
+  const url = 'https://dbs.arms.workers.dev/countries/ps/rand/10'
+  const rs = await fetch(url)
+  if (!rs.ok) {
+    return new Response(JSON.stringify({
+      info: 'NOT OK'
+    }), {
+      status: 500,
+      headers: {
+      "Content-Type": "application/json",
+    },
+    })
+  }
+  const json = await rs.json()
+  return new Response(JSON.stringify(json), {
+    headers: {
+      "Content-Type": "application/json",
+    }
+  })
 }
